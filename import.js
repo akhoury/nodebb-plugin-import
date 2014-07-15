6de645b9b5e2d0f11efdb26566c732bf8fd68311
Aziz Khoury <bentael@gmail.com>
2014-07-15 18:55:47 -0400

    better mongo support, and import is triggers events

--- import.js
+++ import.js
@@ -6,14 +6,14 @@ var argv = require('optimist').argv,
 		if (notice) console.log('\n' + notice);
 
 		console.log(+''
-			+ '\nUsage: node import.js --storage="path/to/storage" --log="debug" --flush --convert="bbcode-to-md"'
-			+ '\n\nthis tool will import your well structured data, into NodeBB database of choice, see readme for files structures'
-			+ '\n-s | --storage : [REQUIRED unless you pass in a --config file with storageDir property in it, but the cmd line one will take precedence] where your storage dir is'
-			+ '\n-l | --log	: [OPTIONAL] log level, defaults to "debug", you can pass multiple comma separated i.e. "info,warn,error"'
-			+ '\n-f | --flush : [OPTIONAL] if you want to flush the db and start over, do not use it if you\'re trying to pick up where you left off'
-			+ '\n-t | --convert : [OPTIONAL] if you want to attempt and convert the post content, and user signatures, currently support "html-to-md" or "bbcode-to-md"'
-			+ '\n-c | --config	: [OPTIONAL] input config file, see ./lib/import.js: Import.config for defaults and structure'
-			+ '\n\n--copy-from-test : [OPTIONAL][DEV-ONLY][IGNORE] I use it to copy ./test/storage to ./storage, so I could test this script easily, ignore it'
+				+ '\nUsage: node import.js --storage="path/to/storage" --log="debug" --flush --convert="bbcode-to-md"'
+				+ '\n\nthis tool will import your well structured data, into NodeBB database of choice, see readme for files structures'
+				+ '\n-s | --storage : [REQUIRED unless you pass in a --config file with storageDir property in it, but the cmd line one will take precedence] where your storage dir is'
+				+ '\n-l | --log	: [OPTIONAL] log level, defaults to "debug", you can pass multiple comma separated i.e. "info,warn,error"'
+				+ '\n-f | --flush : [OPTIONAL] if you want to flush the db and start over, do not use it if you\'re trying to pick up where you left off'
+				+ '\n-t | --convert : [OPTIONAL] if you want to attempt and convert the post content, and user signatures, currently support "html-to-md" or "bbcode-to-md"'
+				+ '\n-c | --config	: [OPTIONAL] input config file, see ./lib/import.js: Import.config for defaults and structure'
+				+ '\n\n--copy-from-test : [OPTIONAL][DEV-ONLY][IGNORE] I use it to copy ./test/storage to ./storage, so I could test this script easily, ignore it'
 		);
 	},
 
@@ -41,7 +41,7 @@ if (config.storageDir) {
 	config.nbb.setup = config.nbb.setup || {};
 	config.nbb.setup.runFlush = argv.f || argv.flush ? true : config.nbb.setup.runFlush;
 
-    config.convert = argv.t || argv.convert || config.convert;
+	config.convert = argv.t || argv.convert || config.convert;
 
 	// dev only
 	if (argv['copy-from-test'])
@@ -50,7 +50,9 @@ if (config.storageDir) {
 	var Import = require(path.resolve('./lib/import.js'));
 	var imprt = new Import(config);
 
-	imprt.start();
+	imprt.on('ready', function() {
+		imprt.start();
+	});
 
 } else {
 	error ('You must provide a storage dir, either in a --config file or using the --storage flag');
