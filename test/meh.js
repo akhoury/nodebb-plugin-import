908de135c9a9f909b496225e5af7c9ba671af1b3
Aziz Khoury <bentael@gmail.com>
2014-07-17 18:30:30 -0400

    Update meh.js

--- test/meh.js
+++ test/meh.js
@@ -2,6 +2,11 @@ var fs = require('fs-extra');
 // copy my test data over, so they won't get overwritten, and I get to use them over and over
 fs.copySync('../test/storage', '../storage');
 
-var Import = require('../lib/import.js');
+var Import = require(path.resolve('./lib/import.js'));
+var imprt = new Import(config);
 
-new Import({}).start();
+imprt.on('init.done', function(){
+	// todo: overwrite dispatcher scope to the Import instance
+	imprt.start();
+});
+imprt.init();
