8da8f01388168bb590acb25847d184ede9fa5e23
Aziz Khoury <akhoury@azizs-mac-mini.home>
2014-07-20 10:40:28 -0400

    better parse handling

--- parse.js
+++ parse.js
@@ -1,19 +1,28 @@
 var Entities = require('html-entities').AllHtmlEntities;
 var entities = new Entities();
 
-
-
-// this is an extra parse function that executes on the post content and user signature
+// this is 2 extra parse functions that executes on the posts content and users signatures
 // for you to customize if you need to do anything with them before importing into the NodeBB db
-// however, this executes after the "convert" methods, i.e.  "bbcode-to-md" and "html-to-md"
-module.exports = function( content ){
-    content = content || '';
+module.exports = {
 
-    // do whatever you want with the content, but remember to return it.
+    // this executes BEFORE the "convert" methods, i.e.  "bbcode-to-md" and "html-to-md"
+    before: function( content ){
+        content = content || '';
 
-    // here's an example on to "decode" html entities if you need to
-    // content = entities.decode(content); // this will convert "you&#39;re fat" to "you're fat"
+        // do whatever you want with the content, but remember to return it.
 
-    // leave that statement in
-    return content;
+        // here's an example on to "decode" html entities if you need to
+        // content = entities.decode(content); // this will convert "you&#39;re fat" to "you're fat"
+
+        // leave that statement in
+        return content;
+    },
+
+    // this executes AFTER the "convert" methods, i.e.  "bbcode-to-md" and "html-to-md"
+    after: function( content ) {
+        // do whatever you want with the content, but remember to return it.
+
+        // leave that statement in
+        return content;
+    }
 };
