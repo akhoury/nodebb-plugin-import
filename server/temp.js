59ecd849ccfc281ef16ed3229c7c65d398739b5a
Aziz Khoury <bentael@gmail.com>
2014-10-30 13:43:13 -0400

    Update temp.js

--- server/temp.js
+++ server/temp.js
@@ -2,16 +2,35 @@ var exporter  = require('./exporter.js');
 
 exporter.once('exporter.ready', function() {
     var c = 1;
-    exporter.exportUsers(function(err, map, arr, nextBatch) {
-        console.log('outertest', err, Object.keys(map), arr.length, c++);
-        // setTimeout(nextBatch, 2500);
-    },
-    {
-        batch: 5
-    },
-    function() {
-        console.log('\n\n\n\nim done');
-        process.exit();
+    var batchsize = 25000;
+    
+    async.series([
+        function(nextExport){
+            exporter.exportUsers(function(err, map, arr, nextBatch) {
+                console.log('gotusers', err, Object.keys(map), arr.length, c++);
+                nextBatch();
+            },{batch: batchsize},function() {console.log('usersdone');nextExport();}); 
+        },
+        function(nextExport){
+            exporter.exportCategories(function(err, map, arr, nextBatch) {
+                console.log('gotcategories', err, Object.keys(map), arr.length, c++);
+                nextBatch();
+            },{batch: batchsize},function() {console.log('categoriesdone');nextExport();}); 
+        },
+        function(nextExport){
+            exporter.exportTopics(function(err, map, arr, nextBatch) {
+                console.log('gottopics', err, Object.keys(map), arr.length, c++);
+                nextBatch();
+            },{batch: batchsize},function() {console.log('topicsdone');nextExport();}); 
+        },
+        function(nextExport){
+            exporter.exportPosts(function(err, map, arr, nextBatch) {
+                console.log('gotposts', err, Object.keys(map), arr.length, c++);
+                nextBatch();
+            },{batch: batchsize},function() {console.log('postsdone');nextExport();}); 
+        }
+        ], function() {
+        console.log('done');
     });
 });
 
